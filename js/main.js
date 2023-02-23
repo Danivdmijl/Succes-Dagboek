92e17c1966c2df62151cf95612c26abbcb0d4a5c
Danivdmijl <32570@ma-web.nl>
2023-02-23 15:18:15 +0100

    print fixed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
-console.log("Test voor de JS");
+// window.jsPDF = window.jspdf.jsPDF;
+// var docPDF = new jsPDF();
+// function print(){
+// var elementHTML = document.querySelector("#printTable");
+// docPDF.html(elementHTML, {
+//  callback: function(docPDF) {
+//   docPDF.save('SuccesDagboek.pdf');
+//  },
+//  x: 15,
+//  y: 100,
+//  width:230,
+//  windowWidth: 1300
+// });
+// }
+
+function PrintClicked() {
+window.jsPDF = window.jspdf.jsPDF;
+var docPDF = new jsPDF();
+function print(){
+var elementHTML = document.querySelector("#printTable");
+docPDF.html(elementHTML, {
+ callback: function(docPDF) {
+  docPDF.save('SuccesDagboek.pdf');
+ },
+ x: 15,
+ y: 100,
+ width:230,
+ windowWidth: 1300
+});
+}
+}
